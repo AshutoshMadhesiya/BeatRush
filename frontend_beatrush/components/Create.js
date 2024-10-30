7e094970c3c9baf20656887f0ea22ac12adbd6af
PhoeniX <rpx03775@gmail.com>
2024-10-30 14:19:54 +0530

    fixed bugs in routes

--- frontend_beatrush/components/Create.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-function Create() {
-    return <h2>Create Section</h2>
-}
-
-export default Create()
--- frontend_beatrush/src/App.js
+++ frontend_beatrush/src/App.js
@@ -1,10 +1,11 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../components/Home';
-import Profile from '../components/profile';
-import Login from '../components/Login';
-import Create from '../components/Create';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/Home';
+import Profile from './components/profile';
+import Login from './components/Login';
+import Create from './components/Create';
 
 function App() {
     return (
@@ -13,11 +14,11 @@ function App() {
             <nav>
                 <Link to="/">Home</Link> | 
                 <Link to="/login">Login</Link> | 
-                <Link to="/create">create</Link>
+                <Link to="/create">create</Link> |
                 <Link to="/profile">profile</Link>
             </nav>
             <Routes>
-            <Route path="/" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/create" element={<Create/>}/>
               <Route path="/profile" element={<Profile/>}/>
