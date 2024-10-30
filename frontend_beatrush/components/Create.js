1d96364c1ac3963be5fd6cf577ac6ed126526b8c
PhoeniX <rpx03775@gmail.com>
2024-10-30 13:56:11 +0530

    updated structrure added components

---
+++ frontend_beatrush/components/Create.js
@@ -0,0 +1,6 @@
+import React from 'react'
+function Create() {
+    return <h2>Create Section</h2>
+}
+
+export default Create()
--- frontend_beatrush/src/App.js
+++ frontend_beatrush/src/App.js
@@ -1,11 +1,30 @@
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../components/Home';
+import Profile from '../components/profile';
+import Login from '../components/Login';
+import Create from '../components/Create';
 
 function App() {
-  return (
-    <div className="App">
-      hello start coding.....
-    </div>
-  );
+    return (
+        <Router>
+        <div>
+            <nav>
+                <Link to="/">Home</Link> | 
+                <Link to="/login">Login</Link> | 
+                <Link to="/create">create</Link>
+                <Link to="/profile">profile</Link>
+            </nav>
+            <Routes>
+            <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/create" element={<Create/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+            </Routes>
+        </div>
+    </Router>
+    );
 }
 
 export default App;
